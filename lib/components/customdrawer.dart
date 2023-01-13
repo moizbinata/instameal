05b695d1f84aa029d9224e418809bbde52bbc96d
Moiz <abdulmoiz6789@gmail.com>
2023-01-13 10:48:36 +0500

    version 10 changed

--- lib/components/customdrawer.dart
+++ lib/components/customdrawer.dart
@@ -60,7 +60,7 @@ Widget drawer(context) {
                     style: Theme.of(context).textTheme.bodySmall),
                 SizedBox(),
                 SizedBox(),
-                Text("1.0.0+10",
+                Text("1.0.0+11",
                     textAlign: TextAlign.center,
                     style: Theme.of(context).textTheme.bodySmall),
               ],
